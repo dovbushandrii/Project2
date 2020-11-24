834a88e7f453ff687442be1745e8450c85e576a1
AndrewDovbush <dovbush.andrii@gmail.com>
2020-11-24 11:31:54 +0200

    Added KMP algorithms + table print

--- sps.c
+++ sps.c
@@ -4,22 +4,26 @@
 
 typedef struct CellStruct {
 	char* cellText;
+	unsigned sizeOfText;
 }cell;
 
 typedef struct Row {
 	cell* row;
+	unsigned numberOfCells;
 }row;
 
 typedef struct Table {
 	row* table;
+	unsigned numberOfRows;
 }table;
+
 
 /**
 * Returns number of symbols
 * before '\0'. So it returns
 * size of line.
 * In @text is nullptr -
-* returns -1
+* returns 0
 */
 unsigned length(char* text) {
 	if (text) {
@@ -29,11 +33,13 @@ unsigned length(char* text) {
 		}
 		return i;
 	}
-	return -1;
+	return 0;
 }
 
 /**
-* If copy is empty line, it should be nullprt
+* If copy is empty line, it should be nullprt.
+* Allocate memory for @copy
+* pointer. Do not forget to free().
 */
 void copy(char* origin, char* copy) {
 	if (origin != NULL) {
@@ -52,7 +58,7 @@ void copy(char* origin, char* copy) {
 			}
 		}
 		if (copy != NULL) {
-			for (unsigned i = 0; i < sizeOfText + 1; i++) {
+			for (unsigned i = 0; origin[i] ; i++) {
 				copy[i] = origin[i];
 			}
 		}
@@ -83,6 +89,58 @@ char* concantinate(char* A, char* B) {
 }
 
 /**
+* Prefix function for KMP algorithm.
+* Builds array with prefix function
+* values for line @s. Resulted
+* array is in @pi array.
+* @n - size of line.
+* ATTENTION:
+* Size of @pi MUST be at least @n.
+*/
+void prefix_function(char* s, int* pi, int n) {
+	pi[0] = 0;
+	for (size_t i = 1; i < n; ++i)
+	{
+		int j = pi[i - 1];
+		while ((j > 0) && (s[i] != s[j]))
+			j = pi[j - 1];
+		if (s[i] == s[j])
+			++j;
+		pi[i] = j;
+	}
+}
+
+//TODO: update comments in algorithmKMP
+bool algorithmKMP(char* str, char* obr, int* pi) {
+	pi[0] = 0;
+
+	int l;  
+	for (l = 1; obr[l]; ++l)
+	{
+		int j = pi[l - 1];
+		while ((j > 0) && (obr[l] != obr[j]))
+			j = pi[j - 1];	        
+		if (obr[l] == obr[j])  
+			++j;
+		pi[l] = j;
+	}
+	int j = 0; 
+	for (int i = 0; str[i]; ++i)
+	{
+		while ((j > 0) && (str[i] != obr[j]))
+			
+			j = pi[j - 1];   
+
+		if (str[i] == obr[j])
+			++j;              
+		if (j == l)
+			return true;
+	}
+	return false;
+}
+
+
+/**
 * Returns pointer on text is cell
 * with coordinates (@x,@y)
 * @tab - pointer on table
@@ -104,7 +162,32 @@ void setCellText(table* tab, char* text, const unsigned x, const unsigned y) {
 /*
 * TODO -> KMP algorithm
 */
-int main(int argc, char* argv[]) {
 
+void printRow(row* row, const char delim) {
+	if (row != NULL) {
+		for (unsigned i = 0; i < row->numberOfCells; i++) {
+			if (row->row[i].cellText != NULL) 
+				printf("%s", row->row[i].cellText);
+
+			if (i < row->numberOfCells - 1)
+				printf("%c", delim);
+
+			else printf("\n");
+		}
+	}
+}
+
+void printTable(table* tab, const char delim) {
+	if (tab != NULL) {
+		for (unsigned i = 0; i < tab->numberOfRows; i++) {
+			printRow(&tab->table[i], delim);
+		}
+	}
+}
+
+
+
+int main(int argc, char* argv[]) {
+	(void)argc, (void)argv;
 	return 0;
 }
